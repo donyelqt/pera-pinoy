edbd8bc08c8006c21738f40f865ab7c38c174a97
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-11-05 22:08:23 +0800

    "Updated MainScreen widget: changed padding, icon size and color, added welcome text and user name"

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -7,18 +7,31 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           children: [
             Row(
               children: [
                 Container(
-                  width: 40,
-                  height: 40,
+                  width: 60,
+                  height: 60,
                   decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.yellow
-                  ),
+                      shape: BoxShape.circle, color: Colors.redAccent),
+                ),
+                SizedBox(width: 8,),
+                Column(
+                  children: [
+                    Text(
+                      "Welcome to PeraPinoy!",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800
+                      ),
+                    ), 
+                    Text(
+                      "Doniele Arys Antonio"
+                    )
+                  ],
                 )
               ],
             )
