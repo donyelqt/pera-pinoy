a9eb063ccc090c2a616909454d306379912e6819
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-11-06 02:55:26 +0800

    "Refactored MainScreen widget: rearranged layout, added settings icon button, and reformatted code"

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -12,53 +12,64 @@ class MainScreen extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              padding: const EdgeInsets.all(12.0), // Optimized padding around both elements
+              padding: const EdgeInsets.all(
+                  12.0), // Optimized padding around both elements
               decoration: BoxDecoration(
                 color: Colors.black87, // Black background
                 borderRadius: BorderRadius.circular(14), // Rounded corners
               ),
-              child: Row(
+              child: 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Stack(
-                    alignment: Alignment.center,
+                  Row(
                     children: [
-                      Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.red[700],
-                        ),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.red[700],
+                            ),
+                          ),
+                          Icon(
+                            CupertinoIcons.person_fill,
+                            color: Colors.white,
+                            size: 40,
+                          )
+                        ],
                       ),
-                      Icon(
-                        CupertinoIcons.person_fill, 
-                        color: Colors.white,
-                        size: 40,
-                      )
+                      const SizedBox(width: 8),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Align text to the left
+                        children: [
+                          Text(
+                            "Welcome to PeraPinoy!",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text(
+                            "Doniele Arys Antonio",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.redAccent,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  const SizedBox(width: 8),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
-                    children: [
-                      Text(
-                        "Welcome to PeraPinoy!",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white70,
-                        ),
-                      ),
-                      Text(
-                        "Doniele Arys Antonio",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                    ],
-                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(CupertinoIcons.settings_solid, color: Colors.orangeAccent,)),
                 ],
               ),
             )
@@ -68,4 +79,3 @@ class MainScreen extends StatelessWidget {
     );
   }
 }
-
