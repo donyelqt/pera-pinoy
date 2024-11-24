1062e0d863c6d67678fddee4ccab8f3df23951c3
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-11-24 23:25:16 +0800

    "Refactored LoginScreen widget: added SingleChildScrollView, modified text styles, added email and password text fields, updated button styles and layout."

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,158 +7,167 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Image.asset('assets/PeraPinoy!.png', height: 50)),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/PeraPinoy!.png', height: 200),
-            const SizedBox(height: 20),
-            const Text(
-              'PeraPinoy!',
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.w900,
-                fontFamily: 'Poppins',
-                color: Colors.red,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/PeraPinoy!.png', height: 200),
+              const SizedBox(height: 20),
+              const Text(
+                'PeraPinoy!',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Poppins',
+                  color: Colors.red,
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              'YOUR LIFE PARTNER FOR SMART FINANCES!',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                fontFamily: 'Poppins',
-                color: Colors.black87,
+              const SizedBox(height: 5),
+              Text(
+                'YOUR LIFE PARTNER FOR SMART FINANCES!',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Poppins',
+                  color: Colors.black87,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: Container(
+              const SizedBox(height: 15),
+              SizedBox(
+                width: double.infinity,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(color: Colors.grey.shade300),
+                  ),
+                  child: const TextField(
+                    style: TextStyle(color: Colors.black87),
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
                 decoration: BoxDecoration(
-                  color: Colors.black38,
+                  color: Colors.white,
                   borderRadius: BorderRadius.circular(8.0),
+                  border: Border.all(color: Colors.grey.shade300),
                 ),
                 child: const TextField(
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.black87),
                   decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.white),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.grey),
                     border: InputBorder.none,
                     contentPadding: EdgeInsets.all(10.0),
                   ),
+                  obscureText: true,
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.black38,
-                borderRadius: BorderRadius.circular(8.0),
+              const SizedBox(
+                height: 30,
               ),
-              child: const TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.white),
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(10.0),
-                ),
-                obscureText: true,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 60,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.resolveWith<Color>(
-                    (Set<WidgetState> states) {
-                      if (states.contains(WidgetState.hovered)) {
-                        return Colors.orangeAccent; // Hover color
-                      }
-                      return Colors.red; // Default color
-                    },
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                      (Set<WidgetState> states) {
+                        if (states.contains(WidgetState.hovered)) {
+                          return Colors.red.shade700; // Darker red on hover
+                        }
+                        return Colors.red; // Default color
+                      },
+                    ),
+                    foregroundColor: WidgetStateProperty.all(Colors.white),
+                    elevation: WidgetStateProperty.all(2),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
                   ),
-                  foregroundColor: WidgetStateProperty.all(Colors.white),
-                  shape: WidgetStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
+                  onPressed: () {
+                  // Implement login logic here
+                  // On successful login, navigate to the main screen
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                onPressed: () {
-                  // Implement login logic here
-                  // On successful login, navigate to the main screen
-                  Navigator.pushReplacementNamed(context, '/home');
-                },
-                child: const Text(
-                  'Sign In',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
-                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            const Row(
-              children: <Widget>[
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Text("OR"),
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: double.infinity,
-              height: 60,
-              child: ElevatedButton.icon(
-                icon: Image.asset(
-                  'assets/google.png', // Ensure you have this asset
-                  height: 30.0, // Adjusted height // Adjusted width
-                ),
-                label: const Text(
-                  'Sign in with Google',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(Colors.blue),
-                  foregroundColor: WidgetStateProperty.all(Colors.white),
-                  shape: WidgetStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
+              const SizedBox(height: 10),
+              const Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 1,
                     ),
                   ),
-                ),
-                onPressed: () {
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text("OR"),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton.icon(
+                  icon: Image.asset(
+                    'assets/google.png',
+                    height: 30.0,
+                  ),
+                  label: const Text(
+                    'Sign in with Google',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.blue),
+                    foregroundColor: WidgetStateProperty.all(Colors.white),
+                    // elevation: WidgetStateProperty.all(2),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                       // side: BorderSide(color: Colors.grey.shade300),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
                   // Implement login logic here
                   // On successful login, navigate to the main screen
-                  Navigator.pushReplacementNamed(context, '/home');
-                },
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
